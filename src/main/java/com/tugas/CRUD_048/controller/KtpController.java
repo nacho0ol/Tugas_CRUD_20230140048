fee80942bfdbd7660356a741863addbc4826e95c
nachool <nachma.olivia1394@gmail.com>
2026-03-16 12:11:15 +0700

    import anotasi dan ktpservice

--- src/main/java/com/tugas/CRUD_048/controller/KtpController.java
+++ src/main/java/com/tugas/CRUD_048/controller/KtpController.java
@@ -1,5 +1,7 @@
 package com.tugas.CRUD_048.controller;
 
+import com.tugas.CRUD_048.service.KtpService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
