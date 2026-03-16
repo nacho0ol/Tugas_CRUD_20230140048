49f8566771ef3b7a0039ab83bfd833f1ced726da
nachool <nachma.olivia1394@gmail.com>
2026-03-16 12:09:35 +0700

    nambah import untuk anotasi

--- src/main/java/com/tugas/CRUD_048/controller/KtpController.java
+++ src/main/java/com/tugas/CRUD_048/controller/KtpController.java
@@ -1,5 +1,9 @@
 package com.tugas.CRUD_048.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @RequestMapping("/ktp")
 @CrossOrigin(origins = "*")
