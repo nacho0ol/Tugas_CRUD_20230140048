5b0afe99860099b4f049e1c1e22ff29e570a6ee3
nachool <nachma.olivia1394@gmail.com>
2026-03-16 10:29:41 +0700

    first commit

---
+++ src/main/java/com/tugas/CRUD_048/Crud048Application.java
@@ -0,0 +1,13 @@
+package com.tugas.CRUD_048;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Crud048Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Crud048Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/tugas/CRUD_048/Crud048ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tugas.CRUD_048;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Crud048ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
