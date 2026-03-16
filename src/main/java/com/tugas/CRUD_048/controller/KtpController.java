8c529b41e1d81b84a141358ba7953fc9bdb27f9d
nachool <nachma.olivia1394@gmail.com>
2026-03-16 12:15:06 +0700

    GET KTP by id

--- src/main/java/com/tugas/CRUD_048/controller/KtpController.java
+++ src/main/java/com/tugas/CRUD_048/controller/KtpController.java
@@ -5,10 +5,7 @@ import com.tugas.CRUD_048.service.KtpService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,5 +21,11 @@ public class KtpController {
     public ResponseEntity<List<Ktp>> getAllKtp() {
         List<Ktp> listKtp = ktpService.getAllKtp();
         return new ResponseEntity<>(listKtp, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Ktp> getKtpById(@PathVariable Integer id) {
+        Ktp ktp = ktpService.getKtpById(id);
+        return new ResponseEntity<>(ktp, HttpStatus.OK);
     }
 }
